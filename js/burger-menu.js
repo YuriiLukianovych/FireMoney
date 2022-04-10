1d615b548edac6f4b519f92392235da9393841d8
Yurii Lukianovych <38596364+YuriiLukianovych@users.noreply.github.com>
2022-04-11 00:21:27 +0300

    finished animation of show burger-menu

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,8 +1,16 @@
 const menu = document.querySelector('[data-menu]');
 const menuList = document.querySelector('[data-menu-list]');
 const menuBtn = document.querySelector('[data-menu-button');
+const linksList = document.querySelectorAll('[data-link]');
 
 menuBtn.addEventListener('click', () => {
     menu.classList.toggle('show')
     menuBtn.classList.toggle('is-open')
+})
+
+linksList.forEach(link => {
+    link.addEventListener('click', () => {
+        menu.classList.remove('show')
+        menuBtn.classList.remove('is-open')
+    })
 })
