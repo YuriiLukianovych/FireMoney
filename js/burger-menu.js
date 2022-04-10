401fea0ee64fb88c6b3c37060d6949bf4bec26f0
Yurii Lukianovych <38596364+YuriiLukianovych@users.noreply.github.com>
2022-04-11 00:40:43 +0300

    go to sleep

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -6,11 +6,13 @@ const linksList = document.querySelectorAll('[data-link]');
 menuBtn.addEventListener('click', () => {
     menu.classList.toggle('show')
     menuBtn.classList.toggle('is-open')
+    document.body.classList.toggle('is-blocked')
 })
 
 linksList.forEach(link => {
     link.addEventListener('click', () => {
         menu.classList.remove('show')
         menuBtn.classList.remove('is-open')
+        document.body.classList.remove('is-blocked')
     })
 })
