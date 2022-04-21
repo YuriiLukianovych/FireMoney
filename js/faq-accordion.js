f264b642782d740c4354d78597a3b79f675d21d3
Yurii Lukianovych <38596364+YuriiLukianovych@users.noreply.github.com>
2022-04-21 18:41:24 +0300

    work-work-work!!!

---
+++ js/faq-accordion.js
@@ -0,0 +1,12 @@
+const btnList = document.querySelectorAll('.faq__btn');
+console.log(btnList);
+
+// акордеон
+btnList.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        btn.classList.toggle('faq__btn--open');
+         e.target.previousElementSibling.classList.toggle('faq__text--visible');
+         e.target.parentNode.classList.toggle('faq__item--open');
+         e.target.previousElementSibling.previousElementSibling.classList.toggle('faq__title--open');
+    });
+})
