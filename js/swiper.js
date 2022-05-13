da0914906b4f80b3671674cffe71a0eba69b88ab
Yurii Lukianovych <mr.lukyanovich@gmail.com>
2022-05-13 20:00:17 +0300

    finished swiper integration

--- js/swiper.js
+++ js/swiper.js
@@ -1,6 +1,6 @@
 import Swiper from 'https://unpkg.com/swiper@8/swiper-bundle.esm.browser.min.js';
 
-const swiper = new Swiper('.sw1', {
+const swiper = new Swiper('.swiper', {
    // Optional parameters
    loop: true,
    //    slidesPerView: 3,
@@ -10,12 +10,12 @@ const swiper = new Swiper('.sw1', {
       // when window width is >= 320px
       320: {
          slidesPerView: 1,
-         spaceBetween: 20,
+         spaceBetween: 10,
       },
       // when window width is >= 480px
       768: {
          slidesPerView: 2,
-         spaceBetween: 10,
+         spaceBetween: 15,
       },
       // when window width is >= 640px
       1300: {
